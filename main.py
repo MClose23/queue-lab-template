619213bd11ed35a9ddb7615bc949a573142c7189
MClose23 <123100717+MClose23@users.noreply.github.com>
2024-02-15 15:35:18 -0600

    1.0 Commit

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ class Queue():
 
     #REMOVE PASS AND COMPLETE THE FUNCTION
     def push(self, card):
-        pass
+        self.cards.append(card)
+    
 
     #REMOVE PASS AND COMPLETE THE FUNCTION
     def pop(self):
-        pass
+        self.cards.pop(0)
 
 if __name__ == '__main__':
     #REMOVE PASS AND YOUR CODE GOES HERE
-    pass
+    deck = Queue 
+    print(deck.Queue.cards)
+
+
+    
